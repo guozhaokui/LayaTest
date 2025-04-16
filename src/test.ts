51372b61bb033dff88bc4e473d2c5235881d9d57
guozhaokui <guozhaokuiwang@gmail.com>
2025-04-16 17:22:02 +0800

    针对3.3版本的测试

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,19 @@
 
 import { Laya } from "Laya";
 //import test from "./simple1"
-import "./utils/utils_3_2"
+//import "./utils/utils_3_2"
 //import "./utils/utils_3_1"
+import "./utils/utils_3_3"
+import { Config } from "Config";
+
+//如果要canvas.toBlob必须保留buffer，否则会黑屏
+Config.preserveDrawingBuffer=true;
+
+Laya.addInitCallback(()=>{
+    //这时候已经初始化完成，这个值不再能控制canvas的属性，但是设置为false可以保证能clear
+    Config.preserveDrawingBuffer=false;
+})
+
 
 let testfile = window.location.search.substring(1);
 async function testf(){
